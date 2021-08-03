61486defd1977094a81cc27a621e9dae96f04a71
mojotron <igormalj@gmail.com>
2021-08-03 16:51:04 +0200

    Add helper functions

--- js/main.js
+++ js/main.js
@@ -5,33 +5,47 @@ const btnRoll = document.querySelector('.btn-roll');
 const btnHold = document.querySelector('.btn-hold');
 const diceDisplay = document.querySelector('#dice-display');
 
-const player1 = document.querySelector('.player-1');
-const player2 = document.querySelector('.player-2');
-
-const player1total = document.querySelector('#player-1-score');
-const player2total = document.querySelector('#player-2-score');
-
-const player1current = document.querySelector('#current-1-score');
-const player2current = document.querySelector('#current-2-score');
-
 let player1Score = 0;
 let player2Score = 0;
 let currentScore = 0;
 let currentPlayer = 1;
 
-const rollDice = () => Math.trunc(Math.random() * 20) + 1;
+const rollDice = () => Math.trunc(Math.random() * 6) + 1;
 
 const swapPlayer = () => (currentPlayer = currentPlayer === 1 ? 2 : 1);
 
+const displayScore = function (selector, player, score) {
+  document.querySelector(`#${selector}-${player}-score`).textContent = score;
+};
+
 const swapPlayerEffect = function () {
-  player1.classList.toggle('player-active');
-  player2.classList.toggle('player-active');
+  document.querySelector('.player-1').classList.toggle('player-active');
+  document.querySelector('.player-2').classList.toggle('player-active');
 };
 
 btnRoll.addEventListener('click', function () {
   const roll = rollDice();
-  currentScore += roll;
-  document.querySelector(`#current-${currentPlayer}-score`).textContent =
-    currentScore;
   diceDisplay.textContent = roll;
+  if (roll === 1) {
+    currentScore = 0;
+    displayScore('current', currentPlayer, currentScore);
+    swapPlayer();
+    swapPlayerEffect();
+  } else {
+    currentScore += roll;
+    displayScore('current', currentPlayer, currentScore);
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (currentPlayer === 1) {
+    player1Score += currentScore;
+    displayScore('player', currentPlayer, player1Score);
+  } else {
+    player2Score += currentScore;
+    displayScore('player', currentPlayer, player2Score);
+  }
+  currentScore = 0;
+  swapPlayer();
+  swapPlayerEffect();
 });
