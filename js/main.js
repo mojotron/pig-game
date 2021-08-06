57c8ddf1478b045fa8c7cb2b8b23ea17afb55161
mojotron <igormalj@gmail.com>
2021-08-06 11:44:05 +0200

    Finish evaluate function for d20, basic game logic

--- js/main.js
+++ js/main.js
@@ -7,63 +7,62 @@
   const diceDisplay = document.querySelector('#dice-display');
   //Game State
   const score = { player1: 0, player2: 0 };
-  let currentScore = 0;
-  let currentPlayer = 1;
+  let curScore = 0;
+  let curPlayer = 1;
   //Helper functions
   const rollDice = () => Math.trunc(Math.random() * 20) + 1;
-  const switchPlayer = () => (currentPlayer = currentPlayer === 1 ? 2 : 1);
+
+  const switchPlayer = () => (curPlayer = curPlayer === 1 ? 2 : 1);
+
   const swapPlayer = function () {
     switchPlayer();
     document.querySelector('.player-1').classList.toggle('player-active');
     document.querySelector('.player-2').classList.toggle('player-active');
   };
 
+  const lostRoundUpdate = function () {
+    curScore = 0;
+    updatePlayerDom();
+    swapPlayer();
+  };
+
   const displayScore = function (selector, player, score) {
     document.querySelector(`#${selector}-${player}-score`).textContent = score;
   };
-  const renderNewDisplay = function () {
-    displayScore('player', currentPlayer, score[`player${currentPlayer}`]);
-    displayScore('current', currentPlayer, currentScore);
+
+  const updatePlayerDom = function () {
+    displayScore('player', curPlayer, score[`player${curPlayer}`]);
+    displayScore('current', curPlayer, curScore);
   };
+
   const rollEvaluate = function (roll) {
     if (roll === 1) {
-      if (score[`player${currentPlayer}`] > 0)
-        score[`player${currentPlayer}`] = 0;
-      currentScore = 0;
-      renderNewDisplay();
-      swapPlayer();
-    } else if (roll > 1 && roll < 4) {
-      currentScore = 0;
-      renderNewDisplay();
-      swapPlayer();
-    } else if (roll === 5) {
+      if (score[`player${curPlayer}`] > 0) score[`player${curPlayer}`] = 0;
+      lostRoundUpdate();
+    } else if (roll === 2) {
+      lostRoundUpdate();
+    } else if (roll === 3) {
       switchPlayer();
-      const temp = currentScore <= 5 ? roll : currentScore;
-      score[`player${currentPlayer}`] += temp;
-      displayScore('current', currentPlayer, score[`player${currentPlayer}`]);
-      currentScore = 0;
+      score[`player${curPlayer}`] += curScore <= roll ? roll : curScore;
+      curScore = 0;
+      updatePlayerDom();
       switchPlayer();
-      renderNewDisplay();
+      lostRoundUpdate();
     } else if (roll === 19) {
-      //take current score form enemy if enemy does not have enough put him in -
+      const tempScore = curScore <= roll ? roll : curScore;
+      curScore = 0;
       switchPlayer();
-      const temp = currentScore <= roll ? roll : currentScore;
-      score[`player${currentPlayer}`] -= temp;
-      displayScore('player', currentPlayer, score[`player${currentPlayer}`]);
+      score[`player${curPlayer}`] -= tempScore;
+      updatePlayerDom();
       switchPlayer();
-      currentScore += roll;
-      displayScore('current', currentPlayer, currentScore);
+      score[`player${curPlayer}`] += tempScore;
+      updatePlayerDom();
     } else if (roll === 20) {
-      if (currentScore < 10) {
-        currentScore += 20;
-      } else {
-        currentScore *= 2;
-      }
-
-      displayScore('current', currentPlayer, currentScore);
+      curScore < roll ? (curScore += roll * 2) : (curScore *= 2);
+      displayScore('current', curPlayer, curScore);
     } else {
-      currentScore += roll;
-      displayScore('current', currentPlayer, currentScore);
+      curScore += roll;
+      updatePlayerDom();
     }
   };
   //Event handlers
@@ -74,15 +73,22 @@
   });
 
   btnHold.addEventListener('click', function () {
-    score[`player${currentPlayer}`] += currentScore;
-    displayScore('player', currentPlayer, score[`player${currentPlayer}`]);
-    if (score[`player${currentPlayer}`] >= 500) {
+    score[`player${curPlayer}`] += curScore;
+    curScore = 0;
+    updatePlayerDom();
+    if (score[`player${curPlayer}`] >= 100) {
       document.querySelector('.player-active h2').textContent = 'VICTORY';
       return;
     }
-    currentScore = 0;
+    diceDisplay.textContent = '';
     swapPlayer();
   });
 
   btnNewGame.addEventListener('click', function () {});
+
+  //TODO
+  //dice effect and display
+  //reset button and logic
+  //rules or msg system
+  //victory msg
 })();
